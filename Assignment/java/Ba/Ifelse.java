48e6442fd56e07cb1d4a34105bf0d563cd3d1a65
Diksha Thoke <47947450+thokediksha@users.noreply.github.com>
2021-07-12 22:33:19 +0530

    Add files via upload

---
+++ Assignment/java/Ba/Ifelse.java
@@ -0,0 +1,14 @@
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		int number = 31;
+		if(number%2 == 0) {
+    System.out.println("even number");
+		} else {
+	System.out.println("odd number");
+		}
+			
+	}
+
+}
