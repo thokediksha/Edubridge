9ad00438c3ea93210c7534ae3b536bfe6352cb2e
Diksha Thoke <47947450+thokediksha@users.noreply.github.com>
2021-07-12 22:30:25 +0530

    Add files via upload

---
+++ Assignment/java/Ba/For.java
@@ -0,0 +1,12 @@
+class For{
+public static void main(String[] args){
+for(int i=0;i<5;i++){
+for(int j=2*5;j>=0;j--){
+System.out.print(" ");
+}
+for(int j=0;j<=i; j++){
+System.out.print("*");
+}
+System.out.println();
+}
+}
