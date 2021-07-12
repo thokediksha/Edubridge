d9b4ea22c688db5e76845ddb6aa58d79f14a15be
Diksha Thoke <47947450+thokediksha@users.noreply.github.com>
2021-07-12 22:25:13 +0530

    Add files via upload

---
+++ Assignment/java/Ba/Operator.java
@@ -0,0 +1,13 @@
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int a=10;  
+		int b=5;  
+		int c=20;  
+		System.out.println(a<b&&a<c); 
+		System.out.println(a<b&a<c);
+
+	}
+
+}
