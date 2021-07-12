8465104991964ae1c48ff3daeadfc32c06a5e71d
Diksha Thoke <47947450+thokediksha@users.noreply.github.com>
2021-07-12 22:36:15 +0530

    Add files via upload
    Ternary operator

--- Assignment/java/Ba/Operator.java
+++ Assignment/java/Ba/Operator.java
@@ -2,11 +2,10 @@
 public class Operator {
 
 	public static void main(String[] args) {
-		int a=10;  
+		int a=2;  
 		int b=5;  
-		int c=20;  
-		System.out.println(a<b&&a<c); 
-		System.out.println(a<b&a<c);
+		int min=(a<b)?a:b;  
+		System.out.println(min); 
 
 	}
 
