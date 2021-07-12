0566e114d076e35bf696e891235b3b83dc3d11ef
Diksha Thoke <47947450+thokediksha@users.noreply.github.com>
2021-07-12 22:17:41 +0530

    Add files via upload
    Arithmatic Operations done in one program

---
+++ Assignment/java/Ba/Add.java
@@ -0,0 +1,34 @@
+import java.util.*;
+package String;
+class Add{
+public static void main(String s[]){
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter First No ");
+int x=sc.nextInt();
+System.out.print("Enter Second No ");
+int y=sc.nextInt();
+ int z=y+x;
+System.out.print(z); 
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
