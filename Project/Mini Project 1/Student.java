2000bae9f29c51dc74df51bd09f9f8da4f02f1f0
Diksha Thoke <47947450+thokediksha@users.noreply.github.com>
2021-08-21 23:16:33 +0530

    Add files via upload

---
+++ Project/Mini Project 1/Student.java
@@ -0,0 +1,27 @@
+package StudentDetails;
+
+public class Student {
+	private String sname;
+    private String regNo;
+    private long phno;
+    
+     public long getPhno() {
+    	 return phno;
+     }
+     public void setPhno(long phno) {
+    	 this.phno = phno;
+     }
+     public String getSname() {
+    	 return sname;
+     }
+     public void setHname(String sname) {
+    	 this.sname = sname;
+     }
+     public String getRegNo() {
+    	 return regNo;
+     }
+     public void setRegNo(String regNo) {
+    	 this.regNo = regNo;
+     }
+
+}
