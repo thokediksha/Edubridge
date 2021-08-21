3c518855730dc02c592ac881edfc14aa837b528b
Diksha Thoke <47947450+thokediksha@users.noreply.github.com>
2021-08-21 23:14:24 +0530

    Add files via upload

---
+++ Project/Mini Project 1/hostel.java
@@ -0,0 +1,26 @@
+package HostelDetails;
+
+public class hostel {
+        private long Studentcount;
+        private String hname;
+        private long rcount;
+        
+         public long getRcount() {
+        	 return rcount;
+         }
+         public void setRcount(long rcount) {
+        	 this.rcount = rcount;
+         }
+         public String getHname() {
+        	 return hname;
+         }
+         public void setHname(String hname) {
+        	 this.hname = hname;
+         }
+         public long getStudentcount() {
+        	 return Studentcount;
+         }
+         public void setStudentcount(long studentcount) {
+        	 Studentcount = studentcount;
+         }
+}
