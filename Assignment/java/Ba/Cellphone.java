cca9d9b678cc74e6dbc46d51c06a2e70f6ee3818
Diksha Thoke <47947450+thokediksha@users.noreply.github.com>
2021-09-16 09:55:07 +0530

    Create Cellphone.java

---
+++ Assignment/java/Ba/Cellphone.java
@@ -0,0 +1,17 @@
+public class Cellphone
+{
+void ring()
+{
+System.out.println("ringing...");
+}
+void vibrate()
+{
+System.out.println("vibrating...");
+}
+public static void main(String args[])
+{
+Cellphone cp=new Cellphone();
+cp.ring();
+cp.vibrate();
+}
+}
